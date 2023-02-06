1eb951665f2dfefad5b270044b3abc26d91c4ae0
Utkarsh Dubey <Utkarsh.Dubey@Dell.com>
2023-02-06 17:23:41 +0530

    Adds cloning a vol to SRDf vol

--- service/controller.go
+++ service/controller.go
@@ -405,10 +405,6 @@ func (s *service) CreateVolume(
 	if contentSource != nil {
 		switch req.GetVolumeContentSource().GetType().(type) {
 		case *csi.VolumeContentSource_Volume:
-			if replicationEnabled == "true" {
-				log.Error("VolumeContentSource:Clone Volume is not supported with replication")
-				return nil, status.Errorf(codes.InvalidArgument, "VolumeContentSource:Clone Volume is not supported with replication")
-			}
 			srcVolID = req.GetVolumeContentSource().GetVolume().GetVolumeId()
 			if srcVolID != "" {
 				_, symID, SrcDevID, _, _, err = s.parseCsiID(srcVolID)
@@ -620,10 +616,19 @@ func (s *service) CreateVolume(
 				}
 			}
 			if volContent != "" {
-				if srcSnapID != "" && replicationEnabled == "true" {
-					err = s.LinkSRDFVolToSnapshot(ctx, reqID, symID, srcVol.VolumeID, snapID, localProtectionGroupID, localRDFGrpNo, vol, bias, pmaxClient)
-					if err != nil {
-						return nil, err
+				if replicationEnabled == "true" {
+					if srcSnapID != "" {
+						err = s.LinkSRDFVolToSnapshot(ctx, reqID, symID, srcVol.VolumeID, snapID, localProtectionGroupID, localRDFGrpNo, vol, bias, pmaxClient)
+						if err != nil {
+							return nil, err
+						}
+					} else if srcVolID != "" {
+						//Build the temporary snapshot identifier
+						snapID := fmt.Sprintf("%s%s-%d", TempSnap, s.getClusterPrefix(), time.Now().Nanosecond())
+						err = s.LinkSRDFVolToVolume(ctx, reqID, symID, srcVol, vol, snapID, localProtectionGroupID, localRDFGrpNo, "false", pmaxClient)
+						if err != nil {
+							return nil, err
+						}
 					}
 				}
 			}
@@ -705,22 +710,27 @@ func (s *service) CreateVolume(
 
 	// If volume content source is specified, initiate no_copy to newly created volume
 	if contentSource != nil {
-		if replicationEnabled == "true" {
-			if srcSnapID != "" {
-				err = s.LinkSRDFVolToSnapshot(ctx, reqID, symID, srcVol.VolumeID, snapID, localProtectionGroupID, localRDFGrpNo, vol, bias, pmaxClient)
+		if srcVolID != "" {
+			//Build the temporary snapshot identifier
+			snapID := fmt.Sprintf("%s%s-%d", TempSnap, s.getClusterPrefix(), time.Now().Nanosecond())
+			if replicationEnabled == "true" {
+				err = s.LinkSRDFVolToVolume(ctx, reqID, symID, srcVol, vol, snapID, localProtectionGroupID, localRDFGrpNo, "false", pmaxClient)
 				if err != nil {
 					return nil, err
 				}
-			}
-		} else {
-			if srcVolID != "" {
-				//Build the temporary snapshot identifier
-				snapID := fmt.Sprintf("%s%s-%d", TempSnap, s.getClusterPrefix(), time.Now().Nanosecond())
+			} else {
 				err = s.LinkVolumeToVolume(ctx, symID, srcVol, vol.VolumeID, snapID, reqID, pmaxClient)
 				if err != nil {
 					return nil, status.Errorf(codes.Internal, "Failed to create volume from volume (%s)", err.Error())
 				}
-			} else if srcSnapID != "" {
+			}
+		} else if srcSnapID != "" {
+			if replicationEnabled == "true" {
+				err = s.LinkSRDFVolToSnapshot(ctx, reqID, symID, srcVol.VolumeID, snapID, localProtectionGroupID, localRDFGrpNo, vol, bias, pmaxClient)
+				if err != nil {
+					return nil, err
+				}
+			} else {
 				//Unlink all previous targets from this snapshot if the link is in defined state
 				err = s.UnlinkTargets(ctx, symID, SrcDevID, pmaxClient)
 				if err != nil {
@@ -793,7 +803,7 @@ func (s *service) createMetroVolume(ctx context.Context, req *csi.CreateVolumeRe
 	}
 
 	// params from snapshot request
-	var srcSnapID string
+	var srcVolID, srcSnapID string
 	var symmID, SrcDevID, snapID string
 	var srcVol *types.Volume
 	var volContent string
@@ -805,8 +815,17 @@ func (s *service) createMetroVolume(ctx context.Context, req *csi.CreateVolumeRe
 	if contentSource != nil {
 		switch req.GetVolumeContentSource().GetType().(type) {
 		case *csi.VolumeContentSource_Volume:
-			log.Error("VolumeContentSource:Clone Volume is not supported with replication")
-			return nil, status.Errorf(codes.InvalidArgument, "VolumeContentSource:Clone Volume is not supported with replication")
+			srcVolID = req.GetVolumeContentSource().GetVolume().GetVolumeId()
+			if srcVolID != "" {
+				_, symmID, SrcDevID, _, _, err = s.parseCsiID(srcVolID)
+				if err != nil {
+					// We couldn't comprehend the identifier.
+					log.Error("Could not parse CSI VolumeId: " + srcVolID)
+					return nil, status.Error(codes.InvalidArgument, "Source volume identifier not in supported format")
+				}
+				volContent = srcVolID
+			}
+			break
 		case *csi.VolumeContentSource_Snapshot:
 			srcSnapID = req.GetVolumeContentSource().GetSnapshot().GetSnapshotId()
 			if srcSnapID != "" {
@@ -1031,8 +1050,16 @@ func (s *service) createMetroVolume(ctx context.Context, req *csi.CreateVolumeRe
 					if err != nil {
 						return nil, err
 					}
+				} else if srcVolID != "" {
+					//Build the temporary snapshot identifier
+					snapID := fmt.Sprintf("%s%s-%d", TempSnap, s.getClusterPrefix(), time.Now().Nanosecond())
+					err = s.LinkSRDFVolToVolume(ctx, reqID, symID, srcVol, vol, snapID, localProtectionGroupID, localRDFGrpNo, bias, pmaxClient)
+					if err != nil {
+						return nil, err
+					}
 				}
 			}
+
 			log.WithFields(fields).Info("Idempotent volume detected, returning success")
 			vol.VolumeID = fmt.Sprintf("%s-%s:%s-%s:%s", volumeIdentifier, symID, remoteSymID, vol.VolumeID, remoteVolumeID)
 			volResp := s.getCSIVolume(vol)
@@ -1144,6 +1171,13 @@ func (s *service) createMetroVolume(ctx context.Context, req *csi.CreateVolumeRe
 			if err != nil {
 				return nil, err
 			}
+		} else if srcVolID != "" {
+			//Build the temporary snapshot identifier
+			snapID := fmt.Sprintf("%s%s-%d", TempSnap, s.getClusterPrefix(), time.Now().Nanosecond())
+			err = s.LinkSRDFVolToVolume(ctx, reqID, symID, srcVol, vol, snapID, localProtectionGroupID, localRDFGrpNo, bias, pmaxClient)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 
@@ -1174,6 +1208,33 @@ func (s *service) createMetroVolume(ctx context.Context, req *csi.CreateVolumeRe
 	}
 	log.WithFields(fields).Infof("Created volume with ID: %s", volResp.VolumeId)
 	return csiResp, nil
+}
+
+func (s *service) LinkSRDFVolToVolume(ctx context.Context, reqID, symID string, vol, tgtVol *types.Volume, snapID, localProtectionGroupID, localRDFGrpNo string, bias string, pmaxClient pmax.Pmax) error {
+	// Create a snapshot from the Source
+	// Set max 1 hr lifetime for the temporary snapshot
+	log.Debugf("Creating snapshot %s on %s and linking it to %s", snapID, vol.VolumeID, tgtVol.VolumeID)
+	var TTL int64 = 1
+	snapInfo, err := s.CreateSnapshotFromVolume(ctx, symID, vol, snapID, TTL, reqID, pmaxClient)
+	if err != nil {
+		if strings.Contains(err.Error(), "The maximum number of sessions has been exceeded for the specified Source device") {
+			return status.Errorf(codes.FailedPrecondition, "Failed to create snapshot: %s", err.Error())
+		}
+		return err
+	}
+	// Link the Target to the created snapshot
+	err = s.LinkSRDFVolToSnapshot(ctx, reqID, symID, vol.VolumeID, snapID, localProtectionGroupID, localRDFGrpNo, tgtVol, bias, pmaxClient)
+	if err != nil {
+		return err
+	}
+	// Push the temporary snapshot created for cleanup
+	var cleanReq snapCleanupRequest
+	cleanReq.snapshotID = snapInfo.SnapshotName
+	cleanReq.symmetrixID = symID
+	cleanReq.volumeID = vol.VolumeID
+	cleanReq.requestID = reqID
+	snapCleaner.requestCleanup(&cleanReq)
+	return nil
 }
 
 func (s *service) LinkSRDFVolToSnapshot(ctx context.Context, reqID, symID, srcVolID, snapID, localProtectionGroupID, localRDFGrpNo string, tgtVol *types.Volume, bias string, pmaxClient pmax.Pmax) error {
