8093b4b7d3233367e3813308ca3bdedddc59ba7e
tssushma <98810659+tssushma@users.noreply.github.com>
2023-03-03 15:36:49 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,41 +1,29 @@
 #!/bin/bash
-# Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#      http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 export X_CSI_POWERMAX_CONFIG_PATH="deploy/config.yaml"
 
 # Comma separated list of array managed by the driver
-export X_CSI_MANAGED_ARRAYS="000000000001"
+export X_CSI_MANAGED_ARRAYS="000120000548"
 
 # HTTP endpoint of Unisphere
-export X_CSI_POWERMAX_ENDPOINT="https://0.0.0.1:8443"
+export X_CSI_POWERMAX_ENDPOINT="https://10.228.203.210:8443"
 
 # EnvUser is the name of the environment variable used to set the
 # username when authenticating to Unisphere
-export X_CSI_POWERMAX_USER="username"
+export X_CSI_POWERMAX_USER="smc"
 
 # EnvPassword is the name of the environment variable used to set the
-# user's password when authenticating to Unisphere
-export X_CSI_POWERMAX_PASSWORD="password"
+# user'\''s password when authenticating to Unisphere
+export X_CSI_POWERMAX_PASSWORD="smc"
 
 # EnvSkipCertificateValidation  is the name of the environment variable used to specify
-# that Unisphere's certificate chain and hostname should not
+# that Unisphere'\''s certificate chain and hostname should not
 # be verified
 export X_CSI_POWERMAX_SKIP_CERTIFICATE_VALIDATION="true"
 
 # EnvNodeName is the name of the environment variable used to set the
 # hostname where the node service is running
-export X_CSI_POWERMAX_NODENAME=`hostname`
+export X_CSI_POWERMAX_NODENAME=`hostname -s`
 
 # EnvClusterPrefix is the name of the environment variable that is used
 # to specify as a prefix to apply to objects created via this K8s cluster
@@ -50,24 +38,24 @@ export X_CSI_POWERMAX_AUTOPROBE="true"
 # EnvPortGroups is the name of the environment variable that is used
 # to specify a list of Port Groups that the driver can choose from
 # These Port Groups must exist and be populated
-export X_CSI_POWERMAX_PORTGROUPS="iscsi_ports"
+export X_CSI_POWERMAX_PORTGROUPS="csi_pg2"
 
 # Enable/Disable CSI request and response logging
-# setting them to true sets X_CSI_REQ_ID_INJECTION to true 
+# setting them to true sets X_CSI_REQ_ID_INJECTION to true
 export X_CSI_REQ_LOGGING="true"
 export X_CSI_REP_LOGGING="true"
 
 # Variables for the integration test code.
 export CSI_ENDPOINT=`pwd`/unix_sock
-export SYMID="000000000001"
+export SYMID="000120000548"
 export SRPID="SRP_1"
 export SERVICELEVEL="Bronze"
-export LOCALRDFGROUP="000"
-export REMOTESYMID="000000000001"
+export LOCALRDFGROUP="280"
+export REMOTESYMID="000120000549"
 export REMOTESRPID="SRP_1"
-export REMOTESERVICELEVEL="Gold"
+export REMOTESERVICELEVEL="Bronze"
 export X_CSI_ENABLE_BLOCK="true"
-export REMOTERDFGROUP="000"
+export REMOTERDFGROUP="281"
 export X_CSI_REPLICATION_CONTEXT_PREFIX="powermax"
 export X_CSI_REPLICATION_PREFIX="replication.storage.dell.com"
 
@@ -75,6 +63,9 @@ export X_CSI_REPLICATION_PREFIX="replication.storage.dell.com"
 # Valid values are "FC" or "ISCSI" or "". Value "" will choose FC if both are available.
 # This is mainly for testing.
 export X_CSI_TRANSPORT_PROTOCOL=""
+# Setting X_CSI_REVERSE_PROXY_ENABLED tells the driver that the endpoint being connected to is
+# actually the reverse proxy server
+export X_CSI_REVERSE_PROXY_ENABLED=""
 # Set this value to a higher number is a proxy is being used
 export X_CSI_GRPC_MAX_THREADS="50"
 # Set this value to the timeout as: "300ms", "1.5h" or "2h45m".
