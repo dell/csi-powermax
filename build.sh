0a71d13a78dba9c93ca7d01834accccdff3c0a8c
Yamunadevi N Shanmugam <82038610+shanmydell@users.noreply.github.com>
2023-04-09 07:51:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -333,6 +333,7 @@ elif [ "$SOURCE_IMAGE_TYPE" = "ubim" ]; then
    IMAGE_TYPE="ubim"
 elif [ "$SOURCE_IMAGE_TYPE" = "ubimicro" ]; then
    SOURCE_IMAGE_TAG=$UBIMICRO_VERSION
+   IMAGE_TYPE="ubim"
 fi
 
 build_source_image_repo_name
