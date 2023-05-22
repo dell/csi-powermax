b785c93fc2a9da9a37c7d3b2931811de5235a6f9
Yamunadevi N Shanmugam <82038610+shanmydell@users.noreply.github.com>
2023-05-22 17:27:08 +0530

    Update buildubimicro.sh

--- buildubimicro.sh
+++ buildubimicro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-microcontainer=$(buildah from $SOURCE_REPO)
+microcontainer=$(buildah from $1)
 micromount=$(buildah mount $microcontainer)
 dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ e2fsprogs which xfsprogs device-mapper-multipath util-linux -y
 dnf clean all --installroot $micromount
