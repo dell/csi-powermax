5a0ae13fc33aee18ee502a1b25d26decc88c9584
tssushma <98810659+tssushma@users.noreply.github.com>
2023-07-31 17:16:37 +0530

    Update node.go
    Added changes related to formatting issue

--- service/node.go
+++ service/node.go
@@ -1053,7 +1053,7 @@ func (s *service) NodeGetInfo(
 	var maxPowerMaxVolumesPerNode int64
 	labels, err := s.GetNodeLabels()
 	if err != nil {
-		log.Infof("failed to get Node Labels with error", err.Error())
+		log.Infof("failed to get Node Labels with error %s", err.Error())
 	}
 	if val, ok := labels["max-powermax-volumes-per-node"]; ok {
 		maxPowerMaxVolumesPerNode, err = strconv.ParseInt(val, 10, 64)
