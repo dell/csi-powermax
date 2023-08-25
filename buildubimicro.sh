74cc836cb9f0c622db97dacceddfc7749a963602
boyamurthy <92081029+boyamurthy@users.noreply.github.com>
2023-08-25 16:22:03 +0530

    updating dependency packages (#225)

--- buildubimicro.sh
+++ buildubimicro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 microcontainer=$(buildah from $1)
 micromount=$(buildah mount $microcontainer)
-dnf install --installroot $micromount --releasever=9 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ e2fsprogs which xfsprogs device-mapper-multipath util-linux -y
+dnf install --installroot $micromount --releasever=9 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ e2fsprogs nfs-utils nfs4-acl-tools acl which xfsprogs device-mapper-multipath util-linux -y
 dnf clean all --installroot $micromount
 buildah umount $microcontainer
 buildah commit $microcontainer csipowermax-ubimicro
