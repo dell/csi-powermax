5e758c8abbdf13a5071b2c4fa0122c9163414452
Harish P <harish_p4@dell.com>
2024-09-09 16:34:39 +0530

    Feature/minimal CRD (#318)
    * updated code for minimal manifest
    
    * Added configmap to read array details
    
    * Fixed lint issues
    
    Signed-off-by: Harish P <harish_p4@dell.com>
    
    * fixed lint issues
    
    Signed-off-by: Harish P <harish_p4@dell.com>
    
    ---------
    
    Signed-off-by: Harish P <harish_p4@dell.com>

--- service/envvars.go
+++ service/envvars.go
@@ -124,6 +124,9 @@ const (
 	// EnvConfigFilePath is an env variable which contains the full path for the config file
 	EnvConfigFilePath = "X_CSI_POWERMAX_CONFIG_PATH"
 
+	// EnvArrayConfigPath is an env variable which contains the full path for the config file
+	EnvArrayConfigPath = "X_CSI_POWERMAX_ARRAY_CONFIG_PATH"
+
 	// EnvMaxVolumesPerNode specifies maximum number of volumes that controller can publish to the node.
 	EnvMaxVolumesPerNode = "X_CSI_MAX_VOLUMES_PER_NODE"
 
