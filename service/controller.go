fce22b56d80839e52ac879bc730d3433180149f4
Jooseppi Luna <jooseppi_luna@dell.com>
2024-10-10 22:40:56 -0400

    get rid of debug printout

--- service/controller.go
+++ service/controller.go
@@ -3311,10 +3311,6 @@ func (s *service) CreateSnapshot(
 
 	snapID = fmt.Sprintf("%s-%s-%s", snap.SnapshotName, symID, devID)
 	// populate response structure
-	// log.Debugf("vol.FloatCapacityMB = %f, int64(vol.FloatCapacityMB) = %d, MiBSizeInBytes = %d, int64(vol.FloatCapacityMB) * MiBSizeInBytes = %d", 
-	//	   vol.FloatCapacityMB, int64(vol.FloatCapacityMB), MiBSizeInBytes, int64(vol.FloatCapacityMB) * MiBSizeInBytes)
-	log.Debugf("cylinderSizeInBytes = %d, vol.CapacityCYL = %d, int64(cylinderSizeInBytes * vol.CapacityCYL) = %d", 
-		   cylinderSizeInBytes, vol.CapacityCYL, int64(cylinderSizeInBytes * vol.CapacityCYL))
 	snapshot := &csi.Snapshot{
 		SizeBytes:      int64(cylinderSizeInBytes * vol.CapacityCYL),
 		SnapshotId:     snapID,
