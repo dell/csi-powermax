65ef2ecda13cd44e75d9002577e15512e8a8578a
Jooseppi Luna <jooseppi_luna@dell.com>
2024-11-13 14:25:07 -0500

    Update powermax_test.go

--- pkg/symmetrix/powermax_test.go
+++ pkg/symmetrix/powermax_test.go
@@ -18,6 +18,8 @@ import (
 	"testing"
 
 	pmax "github.com/dell/gopowermax/v2"
+	types "github.com/dell/gopowermax/v2/types/v100"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/metadata"
 )
@@ -58,7 +60,7 @@ func TestGetPowerMaxClient(t *testing.T) {
 
 // ctx context.Context, client pmax.Pmax, symID string
 func TestUpdate(t *testing.T) {
-	symmetrixCapability = SymmetrixCapability{
+	symmetrixCapability = types.SymmetrixCapability{
 		SymmetrixID: "fakeSymmetrix",
 		SnapVxCapable: true,
 		RdfCapable: true,
