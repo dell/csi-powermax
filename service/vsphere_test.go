857a6f504bb9180e94a8829699d579ac2b715137
Akshay Saini <akshay.saini@dell.com>
2024-11-21 13:47:22 +0530

    Add UT for GetSCSILuns in vsphere.go

--- service/vsphere_test.go
+++ service/vsphere_test.go
@@ -1,8 +1,14 @@
 package service
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/simulator"
+	"github.com/vmware/govmomi/vim25"
+
+	"golang.org/x/net/context"
 )
 
 func TestGetLocalMAC(t *testing.T) {
@@ -10,4 +16,36 @@ func TestGetLocalMAC(t *testing.T) {
 	if mac != "" {
 		assert.NoError(t, err)
 	}
+}
+
+func TestGetSCSILuns(t *testing.T) {
+	// Test case: Successful retrieval of SCSI Luns
+	t.Run("Successful retrieval", func(t *testing.T) {
+		simulator.Test(func(ctx context.Context, c *vim25.Client) {
+			// Create a mock VMHost
+			mockVMHost := &VMHost{
+				VM:  object.NewVirtualMachine(c, simulator.Map.Any("VirtualMachine").Reference()),
+				Ctx: context.Background(),
+			}
+
+			// Call the function
+			scsiLuns, err := mockVMHost.GetSCSILuns()
+
+			// Assert the results
+			if err != nil {
+				t.Errorf("Expected no error, got %v", err)
+			}
+
+			if len(scsiLuns) != 2 {
+				t.Errorf("Expected 2 SCSI Luns, got %d", len(scsiLuns))
+			}
+
+			for _, scsiLun := range scsiLuns {
+				expectedCanonicalName := map[string]any{"mpx.vmhba0:C0:T0:L0": nil, "mpx.vmhba1:C0:T0:L0": nil}
+				if _, found := expectedCanonicalName[scsiLun.CanonicalName]; !found {
+					t.Errorf("Expected canonical name %s, got %s", expectedCanonicalName, scsiLun.CanonicalName)
+				}
+			}
+		})
+	})
 }
