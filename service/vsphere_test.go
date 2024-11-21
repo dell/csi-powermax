38c54dbdf04fee9e8c131a6b9abf45050cc99c14
Akshay Saini <akshay.saini@dell.com>
2024-11-21 16:45:35 +0530

    Add UT for DetachRDM in vsphere.go

--- service/vsphere_test.go
+++ service/vsphere_test.go
@@ -75,6 +75,30 @@ func TestAttachRDM(t *testing.T) {
 	})
 }
 
+func TestDetachRDM(t *testing.T) {
+	// Test case: Device is not found in the list of available devices
+	t.Run("Device is not found in the list of available devices", func(t *testing.T) {
+		simulator.Test(func(ctx context.Context, c *vim25.Client) {
+			// Create a mock VMHost
+			err, client := createGovmomiClient(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+			mockVMHost := &VMHost{
+				client: client,
+				Ctx:    context.Background(),
+				VM:     object.NewVirtualMachine(c, simulator.Map.Any("VirtualMachine").Reference()),
+			}
+			deviceNAA := "deviceNAA"
+
+			err = mockVMHost.DetachRDM(mockVMHost.VM, deviceNAA)
+			if err != nil {
+				t.Errorf("Expected no error, got %v", err)
+			}
+		})
+	})
+}
+
 func createGovmomiClient(ctx context.Context) (error, *govmomi.Client) {
 	m := simulator.ESX()
 	defer m.Remove()
