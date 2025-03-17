9914d19138af56508cb28a040b2431078f55d38e
alexemc <32580729+alexemc@users.noreply.github.com>
2025-03-17 19:00:42 -0400

    Reworked the workload script to use deployment with soft pod anti-affinity rules to check that with zoning all replicas will still be scheduled on one node.

--- test/multi-az/scripts/workload.sh
+++ test/multi-az/scripts/workload.sh
@@ -17,24 +17,29 @@
 
 APP_NAMESPACE=pmax-az-test
 SIMPLE_APP_IMG=quay.io/dell/container-storage-modules/csi-vxflexos:nightly
-DEFAULT_WAIT_SECONDS=180
+REPLICAS=10
+DEFAULT_WAIT_SECONDS=120
 
+# Main script calls this function to create an app that is a deployment with 10 replicas and 1 volume.
+# This deployment has a soft pod anti-affinity rule to instruct k8s to try and spread replicas evenly across workers.
+# Our zoning configuration, however, should overrule this even scheduling and instead cause all replicas to be scheduled
+# only on worker(s) that match the zone specified in the storage class. So, if we later find any replica on any other worker,
+# that would indicate a test failure.
 function create_app() {
   app_name=$1
   storage_class=$2
-  node_name=$3
 
   print_msg "Creating app $app_name in namespace $APP_NAMESPACE..."
 
   # validate inputs
-  if [ -z "$app_name" ] || [ -z "$node_name" ] || [ -z "$storage_class" ]; then
-    print_err "app name or node name or storage class not specified"
+  if [ -z "$app_name" ] || [ -z "$storage_class" ]; then
+    print_err "app name or storage class not specified"
     return 1
   fi
 
   # check that the app does not exist yet
-  if kubectl -n $APP_NAMESPACE get pod $app_name &>/dev/null; then
-    print_err "cannot create the app $app_name in namespace $APP_NAMESPACE, since it already exists"
+  if ! check_no_deployment $app_name; then
+    print_err "cannot create the app deployment $app_name in namespace $APP_NAMESPACE, since it already exists"
     return 1
   fi
 
@@ -46,43 +51,31 @@ function create_app() {
     fi
   fi
 
-  # create a workload app
-  make_app_spec $app_name $storage_class $node_name | kubectl -n $APP_NAMESPACE create -f -
+  # create the workload app
+  make_app_spec $app_name $storage_class | kubectl -n $APP_NAMESPACE create -f -
 
-  # wait until the app pod is in the Running state
-  if ! wait_pod_ready $app_name; then
-    print_err "timed out waiting for the app pod $app_name in namespace $APP_NAMESPACE to be Running"
+  # wait until the app is up and running
+  if ! wait_app_ready $app_name; then
+    print_err "timed out waiting for the app deployment $app_name in namespace $APP_NAMESPACE to be ready"
     return 1
   fi
 }
 
 function delete_app() {
-  app_name=$1
+  print_msg "Deleting app namespace $APP_NAMESPACE..."
 
-  print_msg "Deleting app $app_name in namespace $APP_NAMESPACE..."
-
-  # validate inputs
-  if [ -z "$app_name" ]; then
-    print_err "app name not specified"
-    return 1
-  fi
-
-  # delete app pod if it exists
-  if kubectl -n $APP_NAMESPACE get pod $app_name &>/dev/null; then
-    make_app_spec $app_name $storage_class $node_name | kubectl -n $APP_NAMESPACE delete -f -
+  # delete app namespace with all its resources if it exists
+  if kubectl get ns $APP_NAMESPACE &>/dev/null; then
+    kubectl delete ns $APP_NAMESPACE
     if [ $? -ne 0 ]; then
-      print_err "failed to delete app $app_name in namespace $APP_NAMESPACE"
+      print_err "failed to delete app namespace $APP_NAMESPACE"
       return 1
     fi
-#    if ! kubectl -n $APP_NAMESPACE delete pod $app_name; then
-#      print_err "failed to delete pod $app_name in namespace $APP_NAMESPACE"
-#      return 1
-#    fi
   fi
 
-  # check that the app pod has been deleted
-  if ! check_no_pod $app_name; then
-    print_err "the app pod $app_name in namespace $APP_NAMESPACE is not fully deleted"
+  # check that the app namespace has been deleted
+  if kubectl get ns $APP_NAMESPACE &>/dev/null; then
+    print_err "the app namespace $APP_NAMESPACE is not fully deleted"
     return 1
   fi
 
@@ -101,8 +94,8 @@ function validate_app() {
 
   # check that the app state is stable, by observing it for 10 seconds
   for i in $(seq 5); do
-    if ! check_pod_ready $app_name; then
-      print_err "app pod $app_name in namespace $APP_NAMESPACE state hasn't been stable for 10 seconds"
+    if ! check_app_ready $app_name; then
+      print_err "app $app_name replicas in namespace $APP_NAMESPACE haven't been stable for 10 seconds"
       return 1
     fi
     sleep 2
@@ -113,27 +106,46 @@ function validate_app() {
 make_app_spec() {
     app_name=$1
     storage_class=$2
-    node_name=$3
 
     cat << EOF
-apiVersion: v1
-kind: Pod
+kind: Deployment
+apiVersion: apps/v1
 metadata:
   name: $app_name
 spec:
-  nodeName: $node_name
-  containers:
-  - name: $app_name
-    image: $SIMPLE_APP_IMG
-    command: ["/bin/sh"]
-    args: ["-c", "tail -f /dev/null"]
-    volumeMounts:
-    - name: pmaxvol
-      mountPath: /data
-  volumes:
-  - name: pmaxvol
-    persistentVolumeClaim:
-      claimName: "$app_name-vol1"
+  selector:
+    matchLabels:
+      app: $app_name
+  replicas: $REPLICAS
+  template:
+    metadata:
+      labels:
+        app: $app_name
+    spec:
+      affinity:
+        podAntiAffinity:
+          preferredDuringSchedulingIgnoredDuringExecution:
+          - weight: 100
+            podAffinityTerm:
+              labelSelector:
+                matchExpressions:
+                - key: app
+                  operator: In
+                  values:
+                  - $app_name
+              topologyKey: kubernetes.io/hostname
+      containers:
+      - name: $app_name
+        image: $SIMPLE_APP_IMG
+        command: ["/bin/sh"]
+        args: ["-c", "tail -f /dev/null"]
+        volumeDevices:
+        - devicePath: "/dev/pmaxdata"
+          name: pmaxvol
+      volumes:
+      - name: pmaxvol
+        persistentVolumeClaim:
+          claimName: "$app_name-vol1"
 ---
 apiVersion: v1
 kind: PersistentVolumeClaim
@@ -141,12 +153,12 @@ metadata:
   name: "$app_name-vol1"
 spec:
   accessModes:
-  - ReadWriteOnce
+  - ReadWriteMany
   resources:
     requests:
       storage: 8Gi
   storageClassName: $storage_class
-  volumeMode: Filesystem
+  volumeMode: Block
 EOF
 }
 
@@ -155,18 +167,12 @@ k8s_count_resource() {
   kubectl -n $APP_NAMESPACE get $resource -o name | wc -l
 }
 
-check_pod_ready() {
-  pod_name=$1
-  res=$(kubectl -n $APP_NAMESPACE get pod $pod_name --no-headers 2>/dev/null | awk '$3=="Running" {print "true"}')
-  [ "$res" == "true" ]
-}
-
-check_no_pod() {
-  pod_name=$1
-  if [ -z "$pod_name" ]; then
-      [ $(k8s_count_resource pod) -eq 0 ]
+check_no_deployment() {
+  name=$1
+  if [ -z "$name" ]; then
+      [ $(k8s_count_resource deployment) -eq 0 ]
   else
-      kubectl -n $APP_NAMESPACE get pod $pod_name -o name >/dev/null 2>&1 && return 1 || return 0
+      kubectl -n $APP_NAMESPACE get deployment $name -o name >/dev/null 2>&1 && return 1 || return 0
   fi
 }
 
@@ -181,12 +187,20 @@ wait_with_timeout() {
   return 1
 }
 
-wait_pod_ready() {
-  pod_name=$1
-  echo -n "waiting pod ready "
-  wait_with_timeout $DEFAULT_WAIT_SECONDS check_pod_ready $pod_name
+check_app_ready() {
+    name=$1
+    # compare 'available' against 'desired'
+    res=$(kubectl -n $APP_NAMESPACE get deployment $name --no-headers 2>/dev/null | awk -v replicas=$REPLICAS '$2==$4"/"$4 && $4==replicas {print "true"}')
+    [ "$res" == "true" ]
 }
 
+wait_app_ready() {
+  name=$1
+  echo -n "waiting deployment ready "
+  wait_with_timeout $DEFAULT_WAIT_SECONDS check_app_ready $name
+}
+
+# DELETE: test calls
 create_app "abtest" "vxflexos" "worker-2-pklwnootxu0ap.domain"
 validate_app "abtest"
 delete_app "abtest"
