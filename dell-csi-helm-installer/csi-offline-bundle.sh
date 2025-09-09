db8acb69e85816fbc6d201942b6993b5c6cdbf04
Aaron Tye <tyeaaron@gmail.com>
2025-09-09 11:44:38 -0400

    Update 2.14 references to 2.15 (#594)

--- dell-csi-helm-installer/csi-offline-bundle.sh
+++ dell-csi-helm-installer/csi-offline-bundle.sh
@@ -237,7 +237,7 @@ DRIVER="csi-powermax"
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 REPODIR="$( dirname "${SCRIPTDIR}" )"
 
-DRIVERVERSION="csi-powermax-2.14.0"
+DRIVERVERSION="csi-powermax-2.15.0"
 
 while getopts "cprv:h" opt; do
   case $opt in
