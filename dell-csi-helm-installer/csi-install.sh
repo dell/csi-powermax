d49db42efaad9c250e67ca873e96b52e901e2a32
hoppea2 <33433874+hoppea2@users.noreply.github.com>
2026-06-10 11:18:06 -0400

    Update csi-install.sh (#618)

--- dell-csi-helm-installer/csi-install.sh
+++ dell-csi-helm-installer/csi-install.sh
@@ -22,6 +22,7 @@ VERIFY=1
 MODE="install"
 DEFAULT_VERSION="v2.17.0"
 WATCHLIST=""
+DRIVER="csi-powermax"
 
 #
 # usage will print command execution help and then exit
@@ -127,6 +128,14 @@ while getopts ":h-:" optchar; do
     ;;
   esac
 done
+
+# Derive helm chart version from DEFAULT_VERSION (single source of truth)
+DRIVERVERSION="${DRIVER}-${DEFAULT_VERSION#v}"
+
+# Allow override via --helm-charts-version
+if [ -n "$HELMCHARTVERSION" ]; then
+  DRIVERVERSION=$HELMCHARTVERSION
+fi
 
 if [ ! -d "$DRIVERDIR/helm-charts" ]; then
 
